97159a90f45ce585e3243c120cb0774741fbdc0e
Karthika Vijayakumar <karthikavijayakumar579@gmail.com>
2021-02-27 17:26:25 +0530

    video player application

---
+++ home.js
@@ -0,0 +1,5 @@
+watch =()=>{
+
+    window.location.href = "video.html"
+
+}
---
+++ share.js
@@ -0,0 +1,19 @@
+share =()=>{
+
+    alert("You Shared this video succesfully")
+ 
+ }
+ 
+ like =()=>{
+ 
+     alert("You Liked this video succesfully")
+  
+  }
+ 
+  
+  dislike =()=>{
+ 
+     alert("You Disliked this video succesfully")
+  
+  }
+ 
